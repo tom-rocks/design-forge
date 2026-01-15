4cd4f05dec8c98be4ed1aac01d35f666935763fb
tom-rocks <118841400+tom-rocks@users.noreply.github.com>
2026-01-14 21:17:18 -0300

    Change 'Output appears here' to 'Ready to cast'

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -588,7 +588,7 @@ export default function App() {
                       exit={{ opacity: 0 }}
                       transition={{ duration: 0.3 }}
                     >
-                      <span>Output appears here</span>
+                      <span>Ready to cast</span>
                     </motion.div>
                   )}
                 </AnimatePresence>
